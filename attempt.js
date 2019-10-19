66ae96b682920af5f5341f6c07dfc7aa435d3fe5
root <root@localhost.localdomain>
2019-10-19 15:18:47 +0000

    Added login page

---
+++ attempt.js
@@ -0,0 +1,6 @@
+var attempt = 3;
+attempt --;
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
